d1656df4d29c5cbd50389419a227b667e352ce71
Shawn.Brown <shawn.c.brown12@gmail.com>
2023-01-12 16:55:43 -0600

    updating several files and uploading scaling exercises

--- modeling.py
+++ modeling.py
@@ -11,7 +11,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix, plot_confusion_matrix
 
-def make_baseline(df, baseline, col):
+def calc_baseline(df, baseline, col):
     '''
     This function is used to create a column within the dataframe to make a baseline column, and then calculate the baseline accuracy. Needs to be optimized more, but functions as is currently. Make sure to use the word 'baseline' when calling function.
     '''
@@ -24,29 +24,37 @@ def make_baseline(df, baseline, col):
     
     print(f'Baseline Accuracy is: {base:.3}')
     
-def xy_train(train, validate, test):
+def xy_subsets(train, validate, test):
     '''
-    This function will separate each of my subsets for the telco dataset (train, validate, and test) and split them further into my x and y subsets for modeling. When running this, be sure to assign each of the six variables in the proper order, otherwise it will almost certainly mess up. (X_train, y_train, X_validate, y_validate, X_test, y_test). NEEDS TO BE REDONE FOR ZILLOW
-    '''
-    
+    This function will separate each of my subsets for the zillow dataset (train, validate, and test) and split them further into my x and y subsets for modeling. When running this, be sure to assign each of the six variables in the proper order, otherwise it will almost certainly mess up. (X_train, y_train, X_validate, y_validate, X_test, y_test). NEEDS TO BE REDONE FOR ZILLOW
+    '''  
     seed = 42
     
-    X_train = train.drop(columns=['contract_type', 
-                                  'internet_service_type', 
-                                  'churn_Yes',
-                                  'customer_id'])
-    y_train = train.churn_Yes
+    X_train = train.drop(columns=['tax_value'])
+    y_train = train.tax_value
 
-    X_validate = validate.drop(columns=['contract_type', 
-                                   'internet_service_type', 
-                                   'churn_Yes',
-                                   'customer_id'])
-    y_validate = validate.churn_Yes
+    X_validate = validate.drop(columns=['tax_value'])
+    y_validate = validate.tax_value
 
-    X_test = test.drop(columns=['contract_type', 
-                                'internet_service_type', 
-                                'churn_Yes',
-                                'customer_id'])
-    y_test = test.churn_Yes
+    X_test = test.drop(columns=['tax_value'])
+    y_test = test.tax_value
     
     return X_train, y_train, X_validate, y_validate, X_test, y_test
+
+def train_vailidate_test_split(df, target):
+    '''
+     This function will take my dataset, turn it into train, validate, and test, and then further split them into the x and y subsets. When running this, be sure to assign each of the variables in the proper order, otherwise it will almost certainly mess up. (X_train, y_train, X_validate, y_validate, X_test, y_test)
+    '''
+    train_validate, test = train_test_split(df, train_size =.8, random_state = 91, stratify = df[target])
+    train, validate = train_test_split(train_validate, train_size = .7, random_state = 91, stratify = train_validate[target])
+    
+    X_train = train.drop(columns=target)
+    y_train = train[target]
+    
+    X_val = validate.drop(columns=target)
+    y_val = validate[target]
+    
+    X_test = test.drop(columns=target)
+    y_test = test[target]
+    
+    return train, validate, test, X_train, y_train, X_val, y_val, X_test, y_test
--- wrangling.py
+++ wrangling.py
@@ -1,22 +1,37 @@
 import os
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from env import host, username, password
 from scipy import stats
 from sklearn.model_selection import train_test_split
 from env import host, username, password
 
+#----------------------------------------------------------    
+
 def get_connection(db, user=username, host=host, password=password):
+    '''
+    This functions imports my credentials for the Codeup MySQL server to be used to pull data
+    '''
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
+#----------------------------------------------------------    
+    
 #simply copied this for the framework, will be changed for other data
 def get_zillow():
     '''
     This function will check locally if there's a zillow.csv file in the local directory, and if not, working with the 
     get_connection function, will pull the zillow dataset from the Codeup MySQL server. After that, it will also save a copy of 
-    the csv locally if there wasn't one, so it doesn't have to run the query each time.
+    the csv locally if there wasn't one, so it doesn't have to run the query each time. Additionally, this will clean up by dataset to be ready to be split and used for modeling.
     '''
     if os.path.isfile('zillow_2017.csv'):
-        return pd.read_csv('zillow_2017.csv')
+        
+        df = pd.read_csv('zillow_2017.csv')
+        df = df.drop(columns='Unnamed: 0')
+
+        return df
+    
     else:
         url = get_connection('zillow')
         query = '''
@@ -26,34 +41,55 @@ def get_zillow():
                 JOIN propertylandusetype USING(propertylandusetypeid)
                 WHERE propertylandusedesc = 'Single Family Residential';
                 '''
-        zillow = pd.read_sql(query, url)
-        zillow.to_csv('zillow_2017.csv')
-        return 
+        df = pd.read_sql(query, url)        
+        df = df.dropna()
 
-def wrangle_zillow(df):
-    '''
-    This function will, using an already existing DataFrame, clean the zillow_2017.csv created with my acquire function to make it ready to be split and explored.
-    '''
-    df = df.drop(columns='Unnamed: 0')
-    df = df.dropna()
+        df.bedroomcnt = df.bedroomcnt.astype(int)
+        df.calculatedfinishedsquarefeet = df.calculatedfinishedsquarefeet.astype(int)
+        df.taxvaluedollarcnt = df.taxvaluedollarcnt.astype(int)
+        df.yearbuilt = df.yearbuilt.astype(int)
+        df.fips = df.fips.astype(int)
     
-    df.bedroomcnt = df.bedroomcnt.astype(int)
-    df.calculatedfinishedsquarefeet = df.calculatedfinishedsquarefeet.astype(int)
-    df.taxvaluedollarcnt = df.taxvaluedollarcnt.astype(int)
-    df.yearbuilt = df.yearbuilt.astype(int)
-    df.fips = df.fips.astype(int)
-    
-    return df
+        df = df.rename(columns= {'bedroomcnt': 'bedrooms',
+                             'bathroomcnt': 'bathrooms',
+                             'calculatedfinishedsquarefeet': 'square_ft',
+                             'taxvaluedollarcnt':'tax_value',
+                             'yearbuilt':'built',
+                             'taxamaount':'taxes'
+                             })        
+        df.to_csv('zillow_2017.csv')
 
-def split_train_test(df, col):
+        return df
+
+#----------------------------------------------------------        
+    
+def train_val_test(df):
     '''
-    This function takes in a DataFrame, along with a target column (variable), and split it into train, validate, test subsets for our modeling phase.
+    This function takes in a DataFrame and splits it into train, validate, test subsets for our modeling phase. Does not take in a stratify option.
     '''
     seed = 42
-    train, val_test = train_test_split(df, train_size=.5, random_state=seed, stratify=df[col])
-    validate, test = train_test_split(val_test, train_size=.6, random_state=seed, stratify=val_test[col])
+    train, val_test = train_test_split(df, train_size=.5, random_state=seed)
+    validate, test = train_test_split(val_test, train_size=.6, random_state=seed)
     
     return train, validate, test
+
+#----------------------------------------------------------    
+
+def compare_plots(transformed_data, train, target):
+    '''
+    This function will take in a train dataset and a transformed train dataset, bin them, and make a basic pairplot to visualize them side by side.
+    '''
+    
+    plt.subplot(121)
+    plt.hist(train[target], bins=25)
+    plt.title('Original Data')
+    plt.show()
+    
+    plt.subplot(122)
+    plt.hist(transformed_data, bins=25)
+    plt.title('Transformed Data')
+    
+#----------------------------------------------------------        
 
 def chi2_report(df, col, target):
     '''
